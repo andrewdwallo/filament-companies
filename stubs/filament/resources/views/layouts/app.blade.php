ae2a770ac76ebbfd33bc6e9005209b969628f22e
Andrew Wallo <andrewdwallo@gmail.com>
2022-12-21 11:33:14 -0500

    modified:   stubs/filament/resources/views/layouts/app.blade.php 	modified:   stubs/filament/resources/views/layouts/guest.blade.php

--- stubs/filament/resources/views/layouts/app.blade.php
+++ stubs/filament/resources/views/layouts/app.blade.php
@@ -2,43 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
+        <style>[x-cloak] { display: none !important; }</style>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
         @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-filament-companies::banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
         @livewireScripts
+        @stack('scripts')
+    </head>
+
+    <body class="antialiased">
+        {{ $slot }}
+
+        @livewire('notifications')
     </body>
 </html>
--- stubs/filament/resources/views/layouts/guest.blade.php
+++ stubs/filament/resources/views/layouts/guest.blade.php
@@ -1,21 +1,50 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@props([
+    'title' => null,
+    'width' => 'md',
+])
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<x-filament::layouts.base :title="$title">
+    <div @class([
+        'filament-login-page flex items-center justify-center min-h-screen bg-gray-100 text-gray-900 py-12',
+        'dark:bg-gray-900 dark:text-white' => config('filament.dark_mode'),
+    ])>
+        <div @class([
+            'w-screen px-6 -mt-16 space-y-8 md:mt-0 md:px-2',
+            match($width) {
+                'xs' => 'max-w-xs',
+                'sm' => 'max-w-sm',
+                'md' => 'max-w-md',
+                'lg' => 'max-w-lg',
+                'xl' => 'max-w-xl',
+                '2xl' => 'max-w-2xl',
+                '3xl' => 'max-w-3xl',
+                '4xl' => 'max-w-4xl',
+                '5xl' => 'max-w-5xl',
+                '6xl' => 'max-w-6xl',
+                '7xl' => 'max-w-7xl',
+                default => $width,
+            },
+        ])>
+            <div @class([
+                'p-8 space-y-4 bg-white/50 backdrop-blur-xl border border-gray-200 shadow-2xl rounded-2xl relative',
+                'dark:bg-gray-900/50 dark:border-gray-700' => config('filament.dark_mode'),
+            ])>
+                <div class="flex justify-center w-full">
+                    <x-filament::brand />
+                </div>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+                @if (filled($title))
+                    <h2 class="text-2xl font-bold tracking-tight text-center">
+                        {{ $title }}
+                    </h2>
+                @endif
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+                <div {{ $attributes }}>
+                    {{ $slot }}
+                </div>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+
+    @livewire('notifications')
+</x-filament::layouts.base>
