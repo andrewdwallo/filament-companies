0c3803ebb67605ff8addb06015a9225608422d3b
Andrew Wallo <andrewdwallo@gmail.com>
2022-12-27 10:45:18 -0500

    - ui fixes 	modified:   resources/views/components/input.blade.php

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-white text-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-white text-gray-900 dark:bg-gray-700 dark:focus:border-primary-500 dark:text-white border-gray-300 focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 focus:ring-opacity-70 rounded-lg shadow-sm']) !!}>
