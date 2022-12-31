6308f43a56f836a1934f6e05d6ece8cfea656237
Andrew Wallo <andrewdwallo@gmail.com>
2022-12-31 07:44:43 -0500

    - update profile photo background color 	modified:   src/HasProfilePhoto.php

--- src/HasProfilePhoto.php
+++ src/HasProfilePhoto.php
@@ -75,7 +75,7 @@ trait HasProfilePhoto
             return mb_substr($segment, 0, 1);
         })->join(' '));
 
-        return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color=7F9CF5&background=EBF4FF';
+        return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color=FFFFFF&background=111827';
     }
 
     /**
