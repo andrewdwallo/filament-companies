540905798f112cb3c37af9d83e30b21df8195bc1
Andrew Wallo <andrewdwallo@gmail.com>
2022-12-31 12:28:58 -0500

    - darkmode support 	modified:   resources/views/components/dropdown-link.blade.php 	modified:   resources/views/components/switchable-company.blade.php 	modified:   stubs/filament/resources/views/navigation-menu.blade.php

--- resources/views/components/switchable-company.blade.php
+++ resources/views/components/switchable-company.blade.php
@@ -1,4 +1,4 @@
-@props(['company', 'component' => 'filament-companies::dropdown-link'])
+@props(['company', 'component' => 'filament::dropdown.list.item'])
 
 <form method="POST" action="{{ route('current-company.update') }}" x-data>
     @method('PUT')
@@ -10,7 +10,7 @@
     <x-dynamic-component :component="$component" href="#" x-on:click.prevent="$root.submit();">
         <div class="flex items-center">
             @if (Auth::user()->isCurrentCompany($company))
-                <svg class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <x-heroicon-s-check-circle wire:target class="mr-2 h-5 w-5 rtl:ml-2 rtl:mr-0 group-hover:text-white group-focus:text-white text-primary-500" />
             @endif
 
             <div class="truncate">{{ $company->name }}</div>
--- stubs/filament/resources/views/navigation-menu.blade.php
+++ stubs/filament/resources/views/navigation-menu.blade.php
@@ -1,68 +1,50 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <!-- Companies Dropdown -->
-                @if (Wallo\FilamentCompanies\FilamentCompanies::hasCompanyFeatures())
-                    <div class="ml-3 relative">
-                        <x-filament-companies::dropdown align="right" width="60">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                                        {{ Auth::user()->currentCompany->name }}
-
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
-
-                            <x-slot name="content">
-                                <div class="w-60">
-                                    <!-- Company Management -->
-                                    <div class="block px-4 py-2 text-xs text-gray-400">
-                                        {{ __('Manage Company') }}
-                                    </div>
-
-                                    <!-- Company Settings -->
-                                    <x-filament-companies::dropdown-link href="{{ route('filament.pages.show') }}">
-                                        {{ __('Company Settings') }}
-                                    </x-filament-companies::dropdown-link>
-
-                                    @can('create', Wallo\FilamentCompanies\FilamentCompanies::newCompanyModel())
-                                        <x-filament-companies::dropdown-link href="{{ route('filament.pages.create') }}">
-                                            {{ __('Create New Company') }}
-                                        </x-filament-companies::dropdown-link>
-                                    @endcan
-
-                                    <div class="border-t border-gray-100"></div>
-
-                                    <!-- Company Switcher -->
-                                    <div class="block px-4 py-2 text-xs text-gray-400">
-                                        {{ __('Switch Companies') }}
-                                    </div>
-
-                                    @foreach (Auth::user()->allCompanies() as $company)
-                                        <x-filament-companies::switchable-company :company="$company" />
-                                    @endforeach
-                                </div>
-                            </x-slot>
-                        </x-filament-companies::dropdown>
-                    </div>
-                @endif
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+<div class="flex justify-end">
+    @if (Wallo\FilamentCompanies\FilamentCompanies::hasCompanyFeatures())
+        <x-filament::dropdown placement="bottom-end">
+            <x-slot name="trigger" class="ml-4">
+                <button @class([
+                    'inline-flex items-center px-3 py-2 border border-transparent text-sm text-gray-800 hover:text-primary-500 leading-4 font-medium rounded-md bg-white hover:bg-gray-50 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition',
+                    'dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-white dark:hover:border-primary-400 dark:text-white dark:hover:text-primary-400' => config('filament.dark_mode'),
+                ])>
+                    {{ Auth::user()->currentCompany->name }}
+                    <x-heroicon-o-check-circle />
                 </button>
-            </div>
-        </div>
-    </div>
-</nav>
+            </x-slot>
+
+            <!-- Company Management -->
+            <x-filament::dropdown.header :icon="'heroicon-s-user-group'">
+                {{ __('Manage Company') }}
+            </x-filament::dropdown.header>
+
+            <!-- Company Settings -->
+            <x-filament::dropdown.list>
+                <x-filament::dropdown.list.item :icon="'heroicon-s-cog'">
+                    <x-filament-companies::dropdown-link href="{{ route('filament.pages.show') }}">
+                        {{ __('Company Settings') }}
+                    </x-filament-companies::dropdown-link>
+                </x-filament::dropdown.list.item>
+
+                @can('create', Wallo\FilamentCompanies\FilamentCompanies::newCompanyModel())
+                    <x-filament::dropdown.list.item :icon="'heroicon-s-plus-circle'">
+                        <x-filament-companies::dropdown-link href="{{ route('filament.pages.create') }}">
+                            {{ __('New Company') }}
+                        </x-filament-companies::dropdown-link>
+                    </x-filament::dropdown.list.item>
+                @endcan
+
+                <div class="border-t border-gray-100"></div>
+
+                <!-- Company Switcher -->
+                <x-filament::dropdown.header :icon="'heroicon-s-adjustments'">
+                    {{ __('Switch Companies') }}
+                </x-filament::dropdown.header>
+
+                <div class="border-t border-gray-100"></div>
+
+                @foreach (Auth::user()->allCompanies() as $company)
+                    <x-filament-companies::switchable-company :company="$company" />
+                @endforeach
+            </x-filament::dropdown.list>
+        </x-filament::dropdown>
+    @endif
+</div>
