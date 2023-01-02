353219664f354d33cad3dc00ef247598b2987f84
Andrew Wallo <andrewdwallo@gmail.com>
2023-01-02 01:10:07 -0500

    - fix page titles 	modified:   stubs/app/Filament/Pages/Companies/Create.php 	modified:   stubs/app/Filament/Pages/Companies/Show.php 	modified:   stubs/app/Filament/Pages/User/ApiTokens.php

--- stubs/app/Filament/Pages/Companies/Create.php
+++ stubs/app/Filament/Pages/Companies/Create.php
@@ -6,6 +6,10 @@ use Filament\Pages\Page;
 
 class Create extends Page
 {
+    protected static ?string $title = 'Create Company';
+
+    protected static ?string $slug = 'create';
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.companies.create';
