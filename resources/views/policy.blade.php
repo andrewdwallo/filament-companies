1c715165d42b4c085d0afe2e30c37fe7774773f7
wallo <andrewdwallo@gmail.com>
2023-08-22 23:34:28 -0400

    remove theme stubs

--- resources/views/policy.blade.php
+++
@@ -1,5 +0,0 @@
-<x-filament-panels::page.simple>
-    <div class="prose dark:prose-invert mt-6 w-full overflow-hidden bg-white p-6 shadow-md dark:bg-gray-800 sm:max-w-2xl sm:rounded-lg">
-        {!! $policy !!}
-    </div>
-</x-filament-panels::page.simple>
