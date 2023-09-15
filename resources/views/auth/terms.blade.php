6061df1d5ce4ced573bff6c572d60ac00d5d0b69
Ash Monsh <wh7r.com@gmail.com>
2023-09-15 17:42:57 +0300

    remove duplicate boxes in policy and terms pages

--- resources/views/auth/terms.blade.php
+++ resources/views/auth/terms.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page.simple>
-    <div class="prose dark:prose-invert mt-6 w-full overflow-hidden bg-white p-6 shadow-md dark:bg-gray-800 sm:max-w-2xl sm:rounded-lg">
+    <div class="prose dark:prose-invert mt-6 w-full overflow-hidden sm:max-w-2xl">
         {!! $terms !!}
     </div>
 </x-filament-panels::page.simple>
